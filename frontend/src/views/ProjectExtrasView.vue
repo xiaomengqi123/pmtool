7f81b849581a8e80f982e00d1eaa65bcd7fb265e
mqtang <mqtang@iflytek.com>
2026-07-24 14:09:33 +0800

    feat: add project document and risk management UI

---
+++ frontend/src/views/ProjectExtrasView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+import { computed } from 'vue';import { useRoute } from 'vue-router';import { useAuthStore } from '../stores/auth';import ProjectExtras from '../components/ProjectExtras.vue'
+const route=useRoute(),auth=useAuthStore(),projectId=computed(()=>Number(route.params.id))
+</script>
+<template><div class="page"><div class="page-header"><h1 class="page-title">项目文档与风险</h1></div><ProjectExtras :project-id="projectId" :is-manager="auth.isManager"/></div></template>
